9695dd16c50b8146e053aca5eeee51e608686221
Emily Chen <ec2805@columbia.edu>
2016-03-20 00:17:21 -0400

    Server checks duplicate login

--- Server.java
+++ Server.java
@@ -2,10 +2,15 @@
  import java.io.*;
  import java.net.*;
  import java.util.*;
+ import java.security.MessageDigest;
+ import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ConcurrentHashMap;
+
  public class Server
  {
    // The ServerSocket we'll use for accepting new connections
-   private static final String credentialFilePath = "/Users/emchen/Dropbox/columbia/computer_networks/programming/chatserver_cli/user_pass.txt";
+   //private static final String credentialFilePath = "/Users/emchen/Dropbox/columbia/computer_networks/programming/chatserver_cli/user_pass.txt";
+   private static final String credentialFilePath = "/home/ec2805/computer_networks/chatserver_cli/user_pass.txt";
    private ServerSocket ss;
    private Map<String, String> credentials;
    private Map<String, HashMap<InetAddress, Integer>> blockedIPs;
@@ -13,6 +18,7 @@
    // help us avoid having to create a DataOutputStream each time
    // we want to write to a stream.
    private Hashtable outputStreams = new Hashtable();
+   private Map<Socket, String> connectedUsers;
 
    // Constructor and while-accept loop all in one.   
    public Server( int port ) throws IOException {
@@ -30,6 +36,7 @@
          System.err.println(e.getMessage());
      }
      blockedIPs = new HashMap<String, HashMap<InetAddress, Integer>>();
+     connectedUsers = new ConcurrentHashMap<Socket, String>();
      // All we have to do is listen
      listen( port );
    }
@@ -44,53 +51,109 @@ private void listen( int port ) throws IOException {
        // Grab the next incoming connection
        Socket s = ss.accept();
        String nameOfLoggedInUser;
-       if ((nameOfLoggedInUser = authenticated(s)) != null)   {
-           System.out.println(nameOfLoggedInUser);
-           /*
-           // Tell the world we've got it
-           System.out.println( "Connection from "+s.getInetAddress() );
-           // Create a DataOutputStream for writing data to the
-           // other side
-           DataOutputStream dout = new DataOutputStream( s.getOutputStream() );
-           // Save this stream so we don't need to make it again
-           outputStreams.put( s, dout );
-           // Create a new thread for this connection, and then forget
-           // about it
+       DataOutputStream dout = null;
+       DataInputStream din = null;
+       try  {
+           dout = new DataOutputStream( s.getOutputStream() );
+            din = new DataInputStream( s.getInputStream() );
+       } catch( IOException ie ) {ie.printStackTrace();}
+    
+        String username = null;
+        try {
+           while (username == null)    {
+                dout.writeUTF( "Username: " );
+                String[] tokens = din.readUTF().trim().split(" ");
+                if (tokens.length > 1)  {
+                    dout.writeUTF("Username may not contain whitespace.");
+                }
+                else if (tokens.length == 1) {
+                    username = tokens[0];
+                }
+            }
+        } catch( IOException ie ) {ie.printStackTrace();}
+       // check for duplicate user login
+        if (dout != null && outputStreams.get(username) != null)   {
+            // send error msg
+            try  {
+                dout.writeUTF("duplicate connection, can't log you in. closing connection.");
+            } catch( IOException ie ) {ie.printStackTrace();}
+                s.close();
+       }
+       // check credentials
+       else {
+           if (authenticated(s, username))   {
+               connectedUsers.put(s, username);
+                outputStreams.put( username, dout );
+                // send welcome msg
+                try  {
+                    dout.writeUTF("Login successful!");
+                } catch( IOException ie ) {ie.printStackTrace();}
+  
+               
+               /*
+               // Tell the world we've got it
+               // Create a DataOutputStream for writing data to the
+               // other side
+               DataOutputStream dout = new DataOutputStream( s.getOutputStream() );
+               // Save this stream so we don't need to make it again
+               
+               // Create a new thread for this connection, and then forget
+               // about it
            new ServerThread( this, s );
            */
+           }
+           else {System.out.println("auth failed");}
        }
-       else {System.out.println("auth failed");}
      }
 }
 
-private String authenticated(Socket s)  { 
-    String username = null;
+static String sha1(String input) throws NoSuchAlgorithmException {
+    MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+    byte[] result = mDigest.digest(input.getBytes());
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < result.length; i++) {
+        sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+    }
+    return sb.toString();
+}
+
+private boolean authenticated(Socket s, String username)  { 
     String password = null;
-    try {
+    boolean pwMatch = false;
+    InetAddress ip = s.getInetAddress();
+    System.out.println(ip);
+    try { 
         DataOutputStream dout = new DataOutputStream( s.getOutputStream() );
         DataInputStream din = new DataInputStream( s.getInputStream() );
-        while (username == null)    {
-            dout.writeUTF( "Username: " );
-            String[] tokens = din.readUTF().trim().split(" ");
-            if (tokens.length > 1)  {
-                dout.writeUTF("Username may not contain whitespace.");
+
+        int maxConsecutiveAttempts = 3;
+        for (int i=0; i<maxConsecutiveAttempts; i++)    {
+            // get SHA-1 hash of password
+            String hashed = null;
+            while (password == null)    {
+                dout.writeUTF( "Password: " );
+                String[] tokens = din.readUTF().trim().split(" ");
+                if (tokens.length > 1)  {
+                    dout.writeUTF("Password may not contain whitespace.");
+                }
+                else if (tokens.length == 1) {
+                    password = tokens[0];
+                }
             }
-            else if (tokens.length == 1) {
-                username = tokens[0];
+            try {
+                hashed = sha1(password);
+            } catch(NoSuchAlgorithmException e) { e.printStackTrace();}
+            if (hashed.equals(credentials.get(username)))    {
+                pwMatch = true;
+                break;
             }
-        }
-        while (password == null)    {
-            dout.writeUTF( "Password: " );
-            String[] tokens = din.readUTF().trim().split(" ");
-            if (tokens.length > 1)  {
-                dout.writeUTF("Password may not contain whitespace.");
-            }
-            else if (tokens.length == 1) {
-                password = tokens[0];
-            }
+            else {password = null;}
         }
     } catch( IOException ie ) { System.out.println( ie ); }
-    return username;
+    if (!(pwMatch)) {
+        // TODO add to list of blocked IPs
+    }
+    return pwMatch;
 }
 
 // Get an enumeration of all the OutputStreams, one for each client
